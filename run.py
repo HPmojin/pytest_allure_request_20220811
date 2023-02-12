0c5891e44c72325d2f69bf28dbe8a981c195a747
mojin <397135766@qq.com>
2023-02-12 13:06:36 +0800

    1.开启发送邮件

--- run.py
+++ run.py
@@ -18,7 +18,7 @@ from common.exchange_data import ExchangeData
 
 def run():
 
-    setting = ReadFile.read_config('$.email') #获取邮件相关配置信息
+    setting = dict(ReadFile.read_config('$.email')) #获取邮件相关配置信息
     try:
         shutil.rmtree('./target') #删除allure历史数据
     except:
@@ -31,7 +31,7 @@ def run():
     copy(Start_server_bat, targetPath) #拷贝 启动服务器脚本(config/Start_server.bat)，由config目录拷贝到target目录下进行压缩打包发送邮件
 
     Files_path='./target'#压缩打包的目录
-    #EmailServe.send_email(setting,Files_path,ExchangeData.get_pytest_summary()) #发送邮件
+    EmailServe.send_email(setting,Files_path,ExchangeData.get_pytest_summary()) #发送邮件
 
 
 if __name__ == '__main__':
